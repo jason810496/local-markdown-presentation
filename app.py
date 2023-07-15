3a1114f016f986b7fd67a79a0784482bfe6afc8d
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-07-15 18:42:18 +0800

    reload files before request

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
 import os
-from flask import Flask , make_response
+from flask import Flask , make_response 
 from views.selectSlides import selectSlidesHtml
 from views.showSlide import showSlideHtml
 
+# Setting variables
 SLIDES_DIR = '_slides'
 PORT = 8888
+
+# Colors for logging messages
+GREEN = '\033[92m'
+BLUE = '\033[94m'
+RED = '\033[91m'
+ENDC = '\033[0m'
 
 app = Flask(__name__ )
 app.static_folder = 'static'
 
-print( 'Current path:', os.path.dirname(__file__) )
+all_slides = []
+def getLocalSlides():
+    global all_slides
+    all_slides = []
+    for root, dirs, files in os.walk(slides_path):
+        for f in files:
+            if f.endswith('.md'):
+                all_slides.append(os.path.join(root, f)[len(slides_path)+1:])
+    print(f'{GREEN}Available slides:{ENDC}\n' , all_slides)
 
 slides_path = os.path.join(os.path.dirname(__file__), SLIDES_DIR)
-
-all_slides = []
-for root, dirs, files in os.walk(slides_path):
-    for f in files:
-        if f.endswith('.md'):
-            all_slides.append(os.path.join(root, f)[len(slides_path)+1:])
-
 print('Slides path:', slides_path)
-print('Available slides:' , all_slides)
 
+getLocalSlides()
+
+@app.before_request
+def before_request():
+    print(f'{BLUE}Update slide_list in before_request{ENDC}')
+    # update slides list before all requests
+    getLocalSlides()
 
 @app.route('/')
 def index():
-    print('index' , selectSlidesHtml)
     # using selectSlideHtml
     slidesDataList = []
     for slide in all_slides:
@@ -53,11 +66,13 @@ def index():
 @app.route('/markdown/<name>')
 def getMarkdownContent(name):
     if name not in all_slides:
+        print(f'{RED}Slide not found:{ENDC}', name)
         return make_response('Not Found', 404)
 
     with open(os.path.join(slides_path, name), 'r') as f:
         content = f.read()
     
+    print(f'{GREEN}GET{ENDC} {name} {GREEN}content{ENDC} ')
     return make_response(content , 200, {'Content-Type': 'text/plain'} )
 
 @app.route('/slide/<name>')
